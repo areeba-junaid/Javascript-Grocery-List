53e24378952d8833ec26b7f15ecd6d49d202a45a
areeba-junaid <79707470+areeba-junaid@users.noreply.github.com>
2023-06-10 11:05:32 +0500

    Initial

---
+++ script.js
@@ -0,0 +1,22 @@
+
+addEventListener('submit',(e)=>{ 
+    e.preventDefault();
+    let items=document.getElementById("items").value;
+    let ul=document.getElementById("list");
+    const l=`<li class="list-items">${items} <img class="pic one"  src="images/bin (1).png" alt="no image">
+    <img class="pic" src="images/checklist (1).png" alt="no image"></li>`;
+    ul.innerHTML+=l;
+});
+
+let todelete = document.getElementsByClassName("one");
+console.log(todelete);
+todelete.forEach(todelete,addEventListener("click",(e)=>{
+    if(e.target.classList.contains ("one") )
+            e.target.parentElement.remove();
+}));
+function Deletion(){
+    all_lists=Array.from(document.getElementsByClassName("list-items"));
+    all_lists.map(all_lists=>{
+        all_lists.remove();
+    })
+}
